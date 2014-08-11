021eaf47360eb76e9ab5064687bd4f605ab5db8a
Andre <Andre@Admins-MacBook-Pro.local>
2014-08-10 21:58:33 -0700

    Checkpoint

--- Amoeba Wars/Amoeba Wars/GameScene.swift
+++ Amoeba Wars/Amoeba Wars/GameScene.swift
@@ -12,8 +12,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let SPACE_COLOR = SKColor.blackColor()
     
+    var ship: Ship! = nil
+    
+    var leftButton = SKSpriteNode()
+    var rightButton = SKSpriteNode()
+    
     // set up your scene here
     override func didMoveToView(view: SKView) {
+        
+        // disable diagnostic messages
+        // self.view.showsFPS = false
+        // self.view.showsNodeCount = false
+        self.view.multipleTouchEnabled = true
         
         // set background color
         backgroundColor = SPACE_COLOR
@@ -25,13 +35,58 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.contactDelegate = self
         
         // create ship
-        var ship = Ship(scene: self)
+        ship = Ship(scene: self)
         ship.setPosition(frame.size.width / 2, y: frame.size.height / 2)
-        ship.setSpeed(10, dy: 0)
+        ship.setSpeed(0, dy: 0)
+        ship.xScale = 1.5
+        ship.yScale = 1.5
         
         // add ship to scene
         addChild(ship)
         
+        
+        // create buttons
+        var leftButtonTexture = SKTexture(imageNamed: "left.button.png")
+        leftButton = SKSpriteNode(texture: leftButtonTexture)
+        leftButton.xScale = 0.75
+        leftButton.yScale = 0.75
+        leftButton.position = CGPoint(x: self.frame.size.width / 15, y: self.frame.size.height / 2.5)
+        leftButton.alpha = 0.4
+        leftButton.physicsBody = SKPhysicsBody(circleOfRadius: leftButton.size.height / 2)
+        leftButton.physicsBody.dynamic = false
+        leftButton.physicsBody.allowsRotation = false
+        
+        addChild(leftButton)
+        
+        var rightButtonTexture = SKTexture(imageNamed: "right.button.png")
+        rightButton = SKSpriteNode(texture: rightButtonTexture)
+        rightButton.xScale = 0.75
+        rightButton.yScale = 0.75
+        rightButton.position = CGPoint(x: self.frame.size.width / 6, y: self.frame.size.height / 4.5)
+        rightButton.alpha = 0.4
+        rightButton.physicsBody = SKPhysicsBody(circleOfRadius: rightButton.size.height / 2)
+        rightButton.physicsBody.dynamic = false
+        rightButton.physicsBody.allowsRotation = false
+        
+        addChild(rightButton)
+        
+        var leftButton2 = SKSpriteNode(texture: leftButtonTexture)
+        leftButton2.xScale = 0.75
+        leftButton2.yScale = 0.75
+        leftButton2.position = CGPoint(x: self.frame.size.width - self.frame.size.width / 15, y: self.frame.size.height / 2.5)
+        leftButton2.alpha = 0.4
+        
+        addChild(leftButton2)
+        
+        var rightButton2 = SKSpriteNode(texture: rightButtonTexture)
+        rightButton2.xScale = 0.75
+        rightButton2.yScale = 0.75
+        rightButton2.position = CGPoint(x: self.frame.size.width - self.frame.size.width / 6, y: self.frame.size.height / 4.5)
+        rightButton2.alpha = 0.4
+        
+        addChild(rightButton2)
+        
+        /*
         // test
         
         var asteroid = Asteroid(scene: self, type: 1)
@@ -55,13 +110,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         alienShip.setPosition(frame.size.width / 2 + 20, y: frame.size.height / 2)
         alienShip.setSpeed(350, dy: 0)
         addChild(alienShip)
-    }
+        */
+        }
     
-    // called when a touch begins
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        var i = 0
         
-        // let location = touch.locationInNode(self)
-        
+        for touch: AnyObject in touches {
+            // check if in left button
+            let location = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(location)
+            
+            if touchedNode == leftButton {
+                ship.physicsBody.angularVelocity = 3
+            } else if touchedNode == rightButton {
+                ship.physicsBody.angularVelocity = -3
+            }
+        }
     }
     
     // called when a touch ends
@@ -72,3 +138,4 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func update(currentTime: CFTimeInterval) {
     }
 }
+
